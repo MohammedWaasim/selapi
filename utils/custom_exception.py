b395948cfa59ab601bcce621c1f48fef3164d86c
MohammedWaasim <mohammed.waasim@gmial.com>
2020-10-29 12:26:42 +0530

    added custom exception

---
+++ utils/custom_exception.py
@@ -0,0 +1,4 @@
+
+class CustomException(Exception):
+    def __init__(self, msg):
+        super().__init__(self,msg)
--- utils/tempareture_comparetor.py
+++ utils/tempareture_comparetor.py
@@ -1,9 +1,12 @@
+import logging
 import pdb
-
+import utils.custom_logger as cl
+from utils.custom_exception import CustomException
 from utils.weather import Weather
 
 
 class TempretureComparetor():
+    log = cl.customLogger(logging.INFO)
     def __init__(self, variance: int):
         self.variance=variance
 
@@ -11,7 +14,8 @@ class TempretureComparetor():
         return abs(w1.temp_c-w2.temp_c)
 
     def compare_temprature(self,w1: Weather,w2: Weather):
+        self.log.info(f"performing comparision of temperature between {w1.temp_c} and {w2.temp_c} with the referance temp of {self.variance}")
         if(self.compare(w1,w2)<=self.variance):
             return True
         else:
-            raise Exception(f"WTF temperature reported by NDTV is {w1.temp_c} and openweatherapi is {w2.temp_c} and it is too huge Shouldn't NDTV be sued for false Weather Reporting!")
+            raise CustomException(f"WTF temperature reported by NDTV is {w1.temp_c} and openweatherapi is {w2.temp_c} and it is too huge Shouldn't NDTV be sued for false Weather Reporting!")
