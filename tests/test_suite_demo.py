d3df3669978b26944c503d3aa64563f270e4dc97
MohammedWaasim <mohammed.waasim@gmial.com>
2020-11-25 16:59:11 +0530

    fixed regression suite

---
+++ tests/test_suite_demo.py
@@ -0,0 +1,14 @@
+# import unittest
+# from tests.home.login_test import LoginTest
+# from tests.courses.register_courses_csv_test import RegisterCoursesCSVDataTests
+# from tests.api_test.library_test import LibraryValidation
+# from tests.ndtv_test.ndtv_test import NdtvWeather
+#
+# tc1=unittest.TestLoader().loadTestsFromTestCase(LoginTest)
+# tc2=unittest.TestLoader().loadTestsFromTestCase(RegisterCoursesCSVDataTests)
+# tc3=unittest.TestLoader().loadTestsFromTestCase(LibraryValidation)
+# tc4=unittest.TestLoader().loadTestsFromTestCase(NdtvWeather)
+#
+# smokeTest=unittest.TestSuite([tc1])
+# regression=unittest.TestSuite([tc1,tc2,tc3,tc4])
+# unittest.TextTestRunner(verbosity=2).run(smokeTest)
