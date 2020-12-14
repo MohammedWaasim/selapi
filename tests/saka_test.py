df9f86017766b7ebb67b030ece7e5d04e8eaa0a9
MohammedWaasim <mohammed.waasim@gmial.com>
2020-12-15 00:17:10 +0530

    added test flow description

--- tests/saka_test.py
+++ tests/saka_test.py
@@ -1,7 +1,7 @@
 import logging
 import pdb
 import time
-
+import cv2
 from utils.read_data import getYamlData
 import pytest
 import unittest
@@ -21,10 +21,18 @@ class Google_Translate(unittest.TestCase):
         self.saka=SakaPage(self.driver)
         self.options=OptionsPage(self.driver)
 
-
-
     @pytest.mark.order(1)
     def test_saka_tabs_switch(self):
+        """
+        launch saka page and save the window handle
+        launch all the sites listed in test_data
+        save test_site[oslash] window handle
+        switch back to saka page and launch saga again
+        select a [test]site from the list of tabs in saga window
+        Note: current window handle is lost since the window switching is performed by saka not by Selenium
+        initiate switching to test site[saved in step3] and take a snapshot to document it in the report.
+        :return:
+        """
         self.driver.get(self.test_data['base_url']+self.test_data['saka_home'])
         default_window=self.driver.current_window_handle
         window_list=[default_window]
@@ -34,15 +42,28 @@ class Google_Translate(unittest.TestCase):
             self.driver.switch_to_window(list(new_window)[0])
             self.saka.wait_for_page_to_load()
             window_list=self.driver.window_handles
+            if(sites==self.test_data['test_tab']):
+                test_window=list(new_window)[0]
         self.driver.switch_to_window(default_window)
         #launching saka page in the default window
         self.driver.get(self.test_data['base_url'] + self.test_data['saka_home'])
         self.saka.wait_for_page_to_load()
         #switching oslash tab
         self.saka.select_tabs_in_saka_window(self.test_data['websites'][self.test_data['test_tab']])
+        #######
+        self.driver.switch_to_window(test_window)
+        self.saka.screenShot("the switched window")
 
     @pytest.mark.order(2)
     def test_setting_bookmark(self):
+        """
+        launch test page[oslash site]
+        bookmark test page by pressing ['Command'+ 'd'] and click on default 'done' button
+        launch saka page in another window and switch to the saka page window
+        switch to select bookmark tab
+        verify test page[oslash] is in the bookmark list
+        :return:
+        """
         self.driver.get(self.test_data['websites'][self.test_data['test_tab']])
         test_window=self.driver.current_window_handle
         self.saka.wait_for_page_to_load()
@@ -52,9 +73,6 @@ class Google_Translate(unittest.TestCase):
         time.sleep(2)
         pyautogui.press('enter')
         self.driver.execute_script(f"window.open('{self.test_data['base_url'] + self.test_data['saka_home']}')")
-        for window in self.driver.window_handles:
-            if (window !=test_window):
-                self.driver.switch_to_window(window)
         [self.driver.switch_to_window(window) for window in self.driver.window_handles if (window !=test_window)]
         self.saka.select_bookmark_tab()
         self.saka.wait_for_bookmarks_to_display()
@@ -62,6 +80,17 @@ class Google_Translate(unittest.TestCase):
 
     @pytest.mark.order(3)
     def test_update_default_mode(self):
+        """
+        This method is used to update the default mode displayed in saka window.
+        steps:
+            launch saka page
+            verify default mode displayed as 'Tabs'
+            launch options page
+            update the default mode to display 'Bookmarks' and save
+            launch saka page again
+            verify updated default mode displayed as 'Bookmarks'
+        :return:
+        """
         self.driver.get(self.test_data['base_url'] + self.test_data['saka_home'])
         self.saka.wait_for_page_to_load()
         assert self.test_data['default_mode']==self.saka.get_mode_displayed()
